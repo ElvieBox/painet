6292858ac358216f2fce66cdea6af3c6f7727053
ElvieBox <elvelynbox@gmail.com>
2024-09-05 19:38:35 -0400

    --

--- test.py
+++ test.py
@@ -8,8 +8,3 @@ brain.prettyPrintNet(True)
 
 # maybe make the af's seperate from the brain
 # -- also think about reliance issues
-
-
-# BIG NOTE: Brain init now uses blank network, which is not included in Brain
-# This makes it reliant on the rest
-# Might be able to fix by having a "generate blank" function
