1a653ec0cb37f6d53b100ab49fd86ce2b8ee058f
ElvieBox <elvelynbox@gmail.com>
2024-09-05 14:01:00 -0400

    Quick notes on test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import painet as pn
 
-brain = pn.Brain()
+brain = pn.Brain(pn.exampleNetwork)
 
 blankNetwork = [
     {
@@ -19,6 +19,12 @@ blankNetwork = [
     ]
 ]
 
+print(brain.Network)
+
 
 # maybe make the af's seperate from the brain
 # -- also think about reliance issues
+
+
+# BIG NOTE: Brain init now uses blank network, which is not included in Brain
+# This makes it reliant on the rest
